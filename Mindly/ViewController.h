557cc9c52de1f32a9b837e60ff28f5f43ba427a0
awanglilong <571275935@qq.com>
2016-05-26 08:59:24 +0800

    Initial Commit

---
+++ Mindly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mindly
+//
+//  Created by longlong on 16/5/26.
+//  Copyright © 2016年 LongLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
